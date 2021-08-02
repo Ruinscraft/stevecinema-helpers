78f57f166ad09c9dc536e4ada5461cfe4f7cd61c
Dexton Anderson <contact@dextonanderson.com>
2021-08-01 21:19:50 -0500

    Sponsor only join check

---
+++ src/main/java/com/stevecinema/helpers/listener/SponsorOnlyJoin.java
@@ -0,0 +1,43 @@
+package com.stevecinema.helpers.listener;
+
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.Node;
+import net.luckperms.api.node.types.PermissionNode;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public class SponsorOnlyJoin implements Listener {
+
+    private LuckPerms luckPerms;
+
+    public SponsorOnlyJoin() {
+        RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+        if (provider != null) {
+            luckPerms = provider.getProvider();
+        }
+    }
+
+    @EventHandler
+    public void onPreJoin(AsyncPlayerPreLoginEvent event) {
+        boolean canJoin = false;
+        User user = luckPerms.getUserManager().getUser(event.getUniqueId());
+
+        for (Node node : user.getNodes()) {
+            if (node instanceof PermissionNode permissionNode) {
+                if (permissionNode.getPermission().contains("group.sponsor")) {
+                    canJoin = true;
+                }
+            }
+        }
+
+        if (!canJoin) {
+            event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_OTHER);
+            event.setKickMessage("Steve Cinema is currently in Sponsor-only BETA");
+        }
+    }
+
+}
